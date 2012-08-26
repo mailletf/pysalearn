3cfce642f9ec3089bd32daf73dac122186e14ebe
François Maillet <fmaillet@gmail.com>
2012-08-26 14:45:43 -0400

    Good version

--- pysalearn/tests.py
+++ pysalearn/tests.py
@@ -22,7 +22,8 @@ class TestValidExtraction(unittest.TestCase):
 
     def test_id_extraction(self):
         report = pysalearn.extract_id_from_msg(self.raw_message, self.reportHeader)
-        print report.reporter_id
+        self.assertEqual(report.reporter_id, "1T5gDL-0005Sc-4O")
+        self.assertEqual(report.reported_id, "1T4ezk-0006c6-Hp")
 
 if __name__ == '__main__':
     unittest.main()
